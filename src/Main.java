32b45afb131fb3b356148b1ed6181f8941dcca74
MailMan <ainddal@gmail.com>
2024-02-20 14:58:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 21428;
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+
+        System.out.println("Бонусных миль начислено: " + miles);
+
+    }
+}
