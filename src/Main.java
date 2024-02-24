d9b7bbaa2dc49f2ef0e3695d7f718129438c4006
MailMan <ainddal@gmail.com>
2024-02-24 13:28:11 +0300

    Upgrade homework 2-1

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 21428;
-        int bonus = 20;
-
-        int miles = ticket / bonus;
-
-        System.out.println("Бонусных миль начислено: " + miles);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
