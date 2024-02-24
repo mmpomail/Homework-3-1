a650497c4d6f085c88d6fb05f6a61a6d26aca3e4
MailMan <ainddal@gmail.com>
2024-02-24 20:32:15 +0300

    Format

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
-    public int calculate (int price) {
+    public int calculate(int price) {
 
-    int miles;
-    miles = price / 20;
-    return miles;
+        int miles;
+        miles = price / 20;
+        return miles;
 
     }
 }
